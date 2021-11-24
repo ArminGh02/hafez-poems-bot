12819a1bff9195daed841b311d7f39d90d457087
ArminGh02 <arminghorbanian02@gmail.com>
2021-11-24 21:43:17 +0330

    Add /favorite command

--- bot.py
+++ bot.py
@@ -24,14 +24,22 @@ from telegram.ext import (
     Updater,
 )
 
-TOKEN = '<PLACE YOUR BOT API TOKEN HERE>'
+TOKEN = '2071076049:AAGhNzpDhqehljJl4k5sqJ06mevrfwRWiWs'
 POEMS_DIRECTORY_NAME = 'divan/'
 POEMS_COUNT = 495
+FAVORITE_POEMS_QUERY = '#favorite_poems'
 SURROUNDED_WITH_DOUBLE_QUOTES = r'"[\u0600-\u06FF\s]+"'
 NO_MATCH_WAS_FOUND = 'هیچ بیتی با کلمات فرستاده شده پیدا نشد.'
 
-reply_with_line: dict[User, bool] = {}
+user_to_favorite_poems: dict[User, list[str]] = {}
+user_to_reply_with_line: dict[User, bool] = {}
 to_invoke: Callable[[], None]
+
+
+def get_poem(poem_number: int) -> str:
+    poem_filename = POEMS_DIRECTORY_NAME + 'ghazal' + str(poem_number) + '.txt'
+    with open(poem_filename, encoding='utf8') as poem:
+        return poem.read()
 
 
 def start(update: Update, context: CallbackContext) -> None:
@@ -46,6 +54,7 @@ def start(update: Update, context: CallbackContext) -> None:
         )
     else:
         help_command(update, context)
+        user_to_favorite_poems[update.effective_user] = []
 
 
 def help_command(update: Update, _: CallbackContext) -> None:
@@ -62,30 +71,36 @@ def help_command(update: Update, _: CallbackContext) -> None:
         'یک بیتش شامل کلمات وارد شده، باشد دریافت خواهی کرد.\n'
         'در ضمن اگر می خواهی کل یک عبارت با هم (و نه تک تک کلماتش)\n'
         'در بیت جستجو شود، آن را درون "" بگذار.\n'
-        'همچنین با زدن دستور /fal یک فال می توانی بگیری.\n',
+        'همچنین با زدن دستور /fal یک فال می توانی بگیری.\n'
+        f'تعداد کاربران: {max(len(user_to_reply_with_line), len(user_to_favorite_poems))}',
         reply_markup=reply_markup
     )
 
 
 def reply_line(update: Update, _: CallbackContext) -> None:
-    reply_with_line[update.effective_user] = True
+    user_to_reply_with_line[update.effective_user] = True
     update.message.reply_text('از این پس در نتیجه جستجو، بیت را دریافت خواهی کرد.✅')
 
 
 def reply_ghazal(update: Update, _: CallbackContext) -> None:
-    reply_with_line[update.effective_user] = False
+    user_to_reply_with_line[update.effective_user] = False
     update.message.reply_text('از این پس در نتیجه جستجو، کل غزل را دریافت خواهی کرد.✅')
 
 
-def fal(update: Update, _: CallbackContext) -> None:
-    update.message.reply_text(find_fal())
+def random_poem_command(update: Update, _: CallbackContext) -> None:
+    update.message.reply_text(get_random_poem())
 
 
-def find_fal() -> str:
-    rand = randrange(1, POEMS_COUNT)
-    ghazal_filename = POEMS_DIRECTORY_NAME + 'ghazal' + str(rand) + '.txt'
-    with open(ghazal_filename, encoding='utf8') as ghazal:
-        return ghazal.read()
+def get_random_poem() -> str:
+    return get_poem(randrange(1, POEMS_COUNT))
+
+
+def list_favorite_poems(update: Update, _: CallbackContext) -> None:
+    keyboard = [
+        [InlineKeyboardButton('لیست غزل های مورد علاقه ❤️', switch_inline_query_current_chat=FAVORITE_POEMS_QUERY)]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text('دکمه زیر را بزن.', reply_markup=reply_markup)
 
 
 def search_words(update: Update, _: CallbackContext) -> None:
@@ -102,10 +117,16 @@ def search_impl(update: Update, to_search: Union[str, list[str]]) -> None:
     def send_results() -> None:
         to_call = index_of_matched_line_string if isinstance(to_search, str) else index_of_matched_line_words
         results = find_results(update, to_search, to_call)
-        for result in results:
-            update.message.reply_text(result)
+        if user_to_reply_with_line[update.effective_user]:
+            for poem in results:
+                update.message.reply_text(poem)
+        else:
+            for poem_number, poem in results:
+                keyboard = [[InlineKeyboardButton('افزودن به غزل های مورد علاقه ❤️', callback_data=str(poem_number))]]
+                reply_markup = InlineKeyboardMarkup(keyboard)
+                update.message.reply_text(poem, reply_markup=reply_markup)
 
-    if update.effective_user not in reply_with_line:
+    if update.effective_user not in user_to_reply_with_line:
         choose_result_mode(update)
         global to_invoke
         to_invoke = send_results
@@ -131,24 +152,24 @@ def find_results(update: Update,
                  to_search: Union[str, list[str]],
                  index_of_matched_line: Union[Callable[[list[str], str], int], Callable[[list[str], list[str]], int]]
                  ) -> list[str]:
-    if update.effective_user not in reply_with_line:
-        reply_with_line[update.effective_user] = True
+    if update.effective_user not in user_to_reply_with_line:
+        user_to_reply_with_line[update.effective_user] = True
 
     results = []
     found_match = False
     for i in range(1, POEMS_COUNT + 1):
-        ghazal_filename = POEMS_DIRECTORY_NAME + 'ghazal' + str(i) + '.txt'
-        with open(ghazal_filename, encoding='utf8') as ghazal:
-            ghazal_text = ghazal.read()
-            lines = ghazal_text.splitlines()
+        poem_filename = POEMS_DIRECTORY_NAME + 'ghazal' + str(i) + '.txt'
+        with open(poem_filename, encoding='utf8') as poem:
+            poem_text = poem.read()
+            lines = poem_text.splitlines()
             j = index_of_matched_line(lines, to_search)
             if j > -1:
                 found_match = True
-                if reply_with_line[update.effective_user]:
+                if user_to_reply_with_line[update.effective_user]:
                     result = lines[j - 1] + '\n' + lines[j] + '\n' + lines[j + 1]
                     results.append(result)
                 else:
-                    results.append(ghazal_text)
+                    results.append((i, poem_text))
 
     if not found_match:
         results.append(NO_MATCH_WAS_FOUND)
@@ -168,44 +189,68 @@ def choose_result_mode(update: Update) -> None:
 def button_pressed(update: Update, _: CallbackContext) -> None:
     query = update.callback_query
     query.answer()
-    text: str
     if query.data == 'line':
-        text = 'در نتیجه جستجو بیت دریافت می شود.'
-        reply_with_line[update.effective_user] = True
+        user_to_reply_with_line[update.effective_user] = True
+        query.edit_message_text(text='در نتیجه جستجو بیت دریافت می شود.')
+        to_invoke()
+    elif query.data == 'ghazal':
+        user_to_reply_with_line[update.effective_user] = False
+        query.edit_message_text(text='در نتیجه جستجو کل غزل دریافت می شود.')
+        to_invoke()
     else:
-        text = 'در نتیجه جستجو کل غزل دریافت می شود.'
-        reply_with_line[update.effective_user] = False
-    query.edit_message_text(text=text)
-    to_invoke()
+        if update.effective_user not in user_to_favorite_poems:
+            user_to_favorite_poems[update.effective_user] = [get_poem(int(query.data))]
+        else:
+            user_to_favorite_poems[update.effective_user].append(get_poem(int(query.data)))
 
 
 def handle_inline_query(update: Update, _: CallbackContext) -> None:
     query = update.inline_query.query
+    user = update.effective_user
 
+    favorite_poems_queried = query == FAVORITE_POEMS_QUERY
     persian_words = r'[\u0600-\u06FF\s]+'
     search_results = []
-    if compile(SURROUNDED_WITH_DOUBLE_QUOTES).match(query):
+    if favorite_poems_queried:
+        if user in user_to_favorite_poems:
+            search_results = user_to_favorite_poems[user]
+    elif compile(SURROUNDED_WITH_DOUBLE_QUOTES).match(query):
         search_results = find_results(update, query[1:-1], index_of_matched_line_string)
     elif compile(persian_words).match(query):
         search_results = find_results(update, query.split(), index_of_matched_line_words)
 
-    results = [
-        InlineQueryResultArticle(
-            id=str(uuid4()),
-            title='فال',
-            input_message_content=InputTextMessageContent(find_fal())
-        ),
-    ]
-    for search_result in search_results:
+    results = []
+    if not favorite_poems_queried:
         results.append(
             InlineQueryResultArticle(
                 id=str(uuid4()),
-                title=search_result[:40] + '...',
-                input_message_content=InputTextMessageContent(search_result)
-            )
+                title='فال',
+                input_message_content=InputTextMessageContent(get_random_poem())
+            ),
         )
+    if favorite_poems_queried or user_to_reply_with_line[user]:
+        for search_result in search_results:
+            results.append(
+                InlineQueryResultArticle(
+                    id=str(uuid4()),
+                    title=search_result[:40] + '...',
+                    input_message_content=InputTextMessageContent(search_result),
+                )
+            )
+    else:
+        for _, search_result in search_results:
+            results.append(
+                InlineQueryResultArticle(
+                    id=str(uuid4()),
+                    title=search_result[:40] + '...',
+                    input_message_content=InputTextMessageContent(search_result),
+                )
+            )
 
-    update.inline_query.answer(results, switch_pm_text='راهنما ❓', switch_pm_parameter='inline-help')
+    if favorite_poems_queried:
+        update.inline_query.answer(results)
+    else:
+        update.inline_query.answer(results, switch_pm_text='راهنما ❓', switch_pm_parameter='inline-help')
 
 
 def main() -> None:
@@ -214,9 +259,10 @@ def main() -> None:
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler('start', start))
     dispatcher.add_handler(CommandHandler('help', help_command))
-    dispatcher.add_handler(CommandHandler('fal', fal))
+    dispatcher.add_handler(CommandHandler('fal', random_poem_command))
     dispatcher.add_handler(CommandHandler('ghazal', reply_ghazal))
     dispatcher.add_handler(CommandHandler('beit', reply_line))
+    dispatcher.add_handler(CommandHandler('favorite', list_favorite_poems))
     dispatcher.add_handler(MessageHandler(Filters.regex(SURROUNDED_WITH_DOUBLE_QUOTES), search_string))
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, search_words))
     dispatcher.add_handler(CallbackQueryHandler(button_pressed))
