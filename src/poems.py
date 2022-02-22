d5e7276f522d6754a745ec48d6916216b7e0d259
ArminGh02 <arminghorbanian02@gmail.com>
2022-02-22 22:32:18 +0330

    Delete a `_get_poem_text` function

--- src/poems.py
+++ src/poems.py
@@ -17,21 +17,19 @@ poems: tuple[Poem, ...]
 def _init() -> None:
     poems_list = [None] * config.POEMS_COUNT
     for i in range(config.POEMS_COUNT):
-        text = _get_poem_text(i + 1)
+        with open(f'divan/ghazal{i + 1}.txt', encoding='utf8') as poem_file:
+            text = poem_file.read()
+
         with open(f'data/poem_{i + 1}_info.json', encoding='utf8') as json_file:
             poem_info = json.load(json_file)
 
         meter = poem_info['meter']
         related_songs = tuple(poem_info['relatedSongs'])
+
         poems_list[i] = Poem(meter, i, related_songs, text)
 
     global poems
     poems = tuple(poems_list)
-
-
-def _get_poem_text(poem_number: int) -> str:
-    with open(f'divan/ghazal{poem_number}.txt', encoding='utf8') as poem_file:
-        return poem_file.read()
 
 
 _init()
