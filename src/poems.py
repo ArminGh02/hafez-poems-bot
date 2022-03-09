81aff6b69a94b37da881922b70bca7f4e672cd76
ArminGh02 <arminghorbanian02@gmail.com>
2022-03-10 02:55:52 +0330

    Fix a problem

--- src/poems.py
+++ src/poems.py
@@ -24,13 +24,15 @@ def _init() -> None:
     for i in range(config.POEMS_COUNT):
         with open(f'divan/ghazal{i + 1}.txt', encoding='utf8') as poem_file:
             text = poem_file.read()
-
         with open(f'data/poem_{i + 1}_info.json', encoding='utf8') as json_file:
             poem_info = json.load(json_file)
-
         meter = poem_info['meter']
-        related_songs = tuple(map(lambda song: Song(song['title'], song['link'], poem_info['relatedSongs'])))
-
+        related_songs = tuple(
+            map(
+                lambda song: Song(song['title'], song['link']),
+                poem_info['relatedSongs'],
+            )
+        )
         poems_list[i] = Poem(meter, i, related_songs, text)
 
     global poems
